d484bfc6eaecd07df386ce4bde1531e077204a67
Felix Geisendörfer <felix@felixge.de>
2021-06-06 16:36:15 +0200

    Init repo

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/felixge/dd-delta-prof
+
+go 1.16
+
+require (
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	gopkg.in/DataDog/dd-trace-go.v1 v1.30.1-0.20210507131049-ae70d0d7c146 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	_ "embed"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
+)
+
+var (
+	//go:embed version.txt
+	rawVersion string
+	version    = strings.TrimSpace(rawVersion)
+)
+
+const service = "dd-delta-prof"
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	err := profiler.Start(
+		profiler.WithService(service),
+		profiler.WithEnv("prod"),
+		profiler.WithVersion(version),
+		profiler.WithProfileTypes(
+			profiler.CPUProfile,
+			profiler.HeapProfile,
+			profiler.BlockProfile,
+			profiler.MutexProfile,
+			profiler.GoroutineProfile,
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer profiler.Stop()
+	return nil
+}
